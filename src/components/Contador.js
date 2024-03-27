587f2801758586007a855db4c0629b1ba6dc77dc
AlexsandroFde <alexsandro.ferreira02@aluno.ifce.edu.br>
2024-03-26 23:49:32 -0300

    update counter

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -1,18 +1,32 @@
 import React, { useState } from "react";
-import { Text, Button } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import PropTypes from 'prop-types';
 import estilo from "./estilo";
 
-export default ({inicial = 0, passo = 1}) => {
+const Contador = ({inicial = 0, passo = 1}) => {
     const [numero, setNumero] = useState(inicial)
 
     const inc = () => setNumero(numero + passo)
-    const dec = () => setNumero(numero - passo)
+    const dec = () => setNumero(numero - passo >= 0 ? numero - passo : 0)
 
     return (
-        <>
+        <View style={estilo.container}>
             <Text style={estilo.txtG}>{numero}</Text>
-            <Button title="+" onPress={inc}/>
-            <Button title="-" onPress={dec}/>
-        </>
+            <View style={estilo.botoesContainer}>
+                <TouchableOpacity style={[estilo.botao, estilo.botaoMais]} onPress={inc}>
+                    <Text style={estilo.botaoTexto}>+</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[estilo.botao, estilo.botaoMenos]} onPress={dec}>
+                    <Text style={estilo.botaoTexto}>-</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
     )
 }
+
+Contador.propTypes = {
+    inicial: PropTypes.number,
+    passo: PropTypes.number
+}
+
+export default Contador;
